5ab01037fd098d3ba804f442e044e101e1c66a94
Alex <31377763+AlexH893@users.noreply.github.com>
2021-08-16 14:23:45 -0500

    navbar fixes, share dropdown added

--- header.js
+++ header.js
@@ -12,16 +12,19 @@ class Header extends HTMLElement {
                <li><a class="link link-ltr" href="index.html" >Home</a></li>
                <li><a class="link link-ltr" href="index.html">Work</a></li>
                <li><a class="link link-ltr" href="index.html">Contact</a></li>
-               <li><a class="link link-ltr" href="index.html"><img src="img/ico/instagram.png"/></a></li>
+               <li><a class="link link-ltr" href="https://www.instagram.com/honeybunnyink/" target="blank"><img src="img/ico/instagram.png"/></a></li>
                <li><a class="link link-ltr" href="index.html"><img src="img/ico/tiktok.png"/></a></li>
-               <li><a class="link link-ltr" href="index.html">Share</a></li>
-
-
-
-
-                <!-- <li><a class="link link-ltr" href="projects.html">Projects</a></li> -->
-
+               <li><a class="link link-ltr" id="share" href="javascript:void(0)" onClick="shareFunction();">Share</a></li>
            </ul>
+           		<li>
+                <div class="dropdown">
+			        <div id="myDropdown" class="dropdown-content">
+			        <a href="#home"><img src="img/ico/facebook.png"/>Share to Facebook</a>
+			        <a href="#about"><img src="img/ico/twitter.png"/>Share to Twitter</a>
+			        <a href="#contact"><img src="img/ico/pinterest.png"/>Post to Pinterest</a>
+			    </div>  
+                </div>  
+                </li>  
         </div>
      </div>`;
 
