94980928cf46dd5d75da4f762bc51380fe2350f1
Alex <31377763+AlexH893@users.noreply.github.com>
2021-08-17 11:06:51 -0500

    Nav Media Queries
    responsive

--- header.js
+++ header.js
@@ -7,16 +7,16 @@ class Header extends HTMLElement {
     connectedCallback() {
 
         this.innerHTML = `
-        <div class="navbar-left">
-           <ul class="navList">
-               <li><a class="link link-ltr" href="index.html" >Home</a></li>
-               <li><a class="link link-ltr" href="index.html">Work</a></li>
-               <li><a class="link link-ltr" href="index.html">Contact</a></li>
-               <li><a class="link link-ltr" href="https://www.instagram.com/honeybunnyink/" target="blank"><img src="img/ico/instagram.png"/></a></li>
-               <li><a class="link link-ltr" href="index.html"><img src="img/ico/tiktok.png"/></a></li>
-               <li><a class="link link-ltr" id="share" href="javascript:void(0)" onClick="shareFunction();">Share</a></li>
-           </ul>
-           		<li>
+        <div class="navbar-left" id="myNav">
+               <a href="index.html" >Home</a>
+               <a href="index.html">Work</a>
+               <a href="index.html">Contact</a>
+               <a href="https://www.instagram.com/honeybunnyink/" target="blank"><img src="img/ico/instagram.png"/></a>
+               <a href="index.html"><img src="img/ico/tiktok.png"/></a>
+               <a id="share" href="javascript:void(0)" onClick="shareFunction();">Share</a>
+               <a href="javascript:void(0);" class="icon" onclick="theFunction()">
+                <i class="fa fa-bars"></i></li>
+           	
                 <div class="dropdown">
 			        <div id="myDropdown" class="dropdown-content">
 			        <a href="#home"><img src="img/ico/facebook.png"/>Share to Facebook</a>
@@ -24,9 +24,7 @@ class Header extends HTMLElement {
 			        <a href="#contact"><img src="img/ico/pinterest.png"/>Post to Pinterest</a>
 			    </div>  
                 </div>  
-                </li>  
-        </div>
-     </div>`;
+            </div>`;
 
     }
 }
