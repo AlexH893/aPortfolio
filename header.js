9cf598c1f1ed1ff0a411dd569b8b41a76d19da78
Alex <31377763+AlexH893@users.noreply.github.com>
2021-08-17 17:33:54 -0500

    file changes, nav change

--- header.js
+++ header.js
@@ -10,7 +10,7 @@ class Header extends HTMLElement {
         <div class="navbar-left" id="myNav">
                <a href="index.html" >Home</a>
                <a href="index.html">Work</a>
-               <a href="index.html">Contact</a>
+               <a href="contact.html">Contact</a>
                <a href="https://www.instagram.com/honeybunnyink/" target="blank"><img src="img/ico/instagram.png"/></a>
                <a href="index.html"><img src="img/ico/tiktok.png"/></a>
                <a id="share" href="javascript:void(0)" onClick="shareFunction();">Share</a>
@@ -19,7 +19,7 @@ class Header extends HTMLElement {
            	
                 <div class="dropdown">
 			        <div id="myDropdown" class="dropdown-content">
-			        <a href="#home"><img src="img/ico/facebook.png"/>Share to Facebook</a>
+                    <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Falexh893.github.io%2FaPortfolio%2F&quote=Ash%20Huppert's%20Portfolio" target="_blank"><img src="img/ico/facebook.png" onclick/>Share to Facebook</a>
 			        <a href="#about"><img src="img/ico/twitter.png"/>Share to Twitter</a>
 			        <a href="#contact"><img src="img/ico/pinterest.png"/>Post to Pinterest</a>
 			    </div>  
